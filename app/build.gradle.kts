bda2a895eb5fe9a029660b1521b6fdbb4afefa3f
Michael <isaevmihail007@gmail.com>
2023-10-01 12:45:55 +0300

    Add ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,10 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation(platform("androidx.compose:compose-bom:2023.06.01"))
 
     implementation("androidx.compose.ui:ui:1.4.3")
---
+++ app/src/main/java/com/example/u_calculator/ui/CalculatorViewModel.kt
@@ -0,0 +1,88 @@
+package com.example.u_calculator.ui
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+class CalculatorViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(
+        HomeUiState()
+    )
+
+    val uiState: StateFlow<HomeUiState> = _uiState
+
+    fun onButtonClick(buttonType: ButtonType) {
+        when (buttonType) {
+            is ButtonType.Calculate -> calculate()
+            is ButtonType.Clear -> clear()
+            is ButtonType.Decimal -> appendDecimal()
+            is ButtonType.Digit -> appendDigit(buttonType.digit)
+            is ButtonType.Operation -> updateOperation(buttonType.operation)
+        }
+    }
+
+    private fun calculate() {
+        _uiState.update { state ->
+            val (operand1, operand2, operation) = state
+            val number1 = operand1.toDouble()
+            val number2 = operand2.toDouble()
+            val result = when (operation) {
+                ButtonOperation.Addition -> number1 + number2
+                ButtonOperation.Subtraction -> number1 - number2
+                ButtonOperation.Multiplication -> number1 * number2
+                ButtonOperation.Division -> number1 / number2
+                ButtonOperation.Percentage -> number1 % number1 // TODO: Percentage takes 1/100 out of operand1
+                null -> ""
+            }
+            state.copy(
+                operand1 = result.toString().take(16)
+            )
+        }
+    }
+
+    private fun clear() {
+        _uiState.update { state ->
+            state.copy(
+                operand1 = "0",
+                operand2 = "0",
+                operation = null
+            )
+        }
+    }
+
+    private fun appendDecimal() {
+        _uiState.update { state ->
+            val (operand1, operand2, operation) = state
+            state.copy(
+                operand1 = if (operation == null && "." !in operand1) "$operand1." else operand1,
+                operand2 = if (operation != null && "." !in operand2) "$operand2." else operand2
+            )
+        }
+    }
+
+    private fun appendDigit(digit: String) {
+        _uiState.update { state ->
+            val (operand1, operand2, operation) = state
+            state.copy(
+                operand1 = if (operation == null) if (operand1 == "0") digit else operand1 + digit else operand1,
+                operand2 = if (operation != null) if (operand2 == "0") digit else operand2 + digit else operand2
+            )
+        }
+    }
+
+    private fun updateOperation(operation: ButtonOperation) {
+        _uiState.update { state ->
+            state.copy(
+                operation = operation
+            )
+        }
+    }
+}
+
+data class HomeUiState(
+    val operand1: String = "0",
+    val operand2: String = "0",
+    val operation: ButtonOperation? = null
+)
